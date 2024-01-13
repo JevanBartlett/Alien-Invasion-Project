6fa6e9ed8c5a8c34ca567051832b701ab75aebfb
JevanBartlett <jevan1984@gmail.com>
2024-01-13 17:01:26 -0600

    fixed bullet sprite

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,8 @@ from Bullet import Bullet
 
 
 class AlienInvasion:
-
-
     """Overall class to manage game assets and behavior """
+
     def __init__(self):
 
         """Initialize the game, and create game resources."""
@@ -19,7 +18,7 @@ class AlienInvasion:
         # Set the background color
         self.settings = Settings()
 
-        #Full Screen mode setting
+        # Full Screen mode setting
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
@@ -34,26 +33,26 @@ class AlienInvasion:
         while True:
             print("Checking events")
             self._check_events()
-            print ("updating ship")
+            print("updating ship")
             self.ship.update()
             print("updating screen")
             self._update_bullets()
             print("generating bullets")
             self._update_screen()
-            print ("checking framerate")
+            print("checking framerate")
             self.clock.tick(60)
 
-    def _check_events (self):
-        #Respond to keypresses and mouse events
+    def _check_events(self):
+        # Respond to keypresses and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                self._check_keyup_events (event)
+                self._check_keyup_events(event)
 
-    def _check_keydown_events (self, event):
+    def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
@@ -74,20 +73,24 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
     def _update_bullets(self):
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         print(len(self.bullets))
+
     def _update_screen(self):
-            self.screen.fill(self.settings.bg_color)
-            for bullet in self.bullets.sprites():
-                bullet.draw_bullet()
-            self.ship.blitme()
-  #Make the most recently draw screen visible.
-            pygame.display.flip()
-            self.clock.tick(60)
+        self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()
+        # Make the most recently draw screen visible.
+        pygame.display.flip()
+        self.clock.tick(60)
+
+
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
