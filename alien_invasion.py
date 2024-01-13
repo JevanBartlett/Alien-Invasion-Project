bbbd90833954f5a406c01c9297b6f6478b28bf09
JevanBartlett <jevan1984@gmail.com>
2024-01-13 15:23:57 -0600

    newest changes

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,7 +22,7 @@ class AlienInvasion:
         #Full Screen mode setting
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_width = self.screen.get_rect().height
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -37,9 +37,9 @@ class AlienInvasion:
             print ("updating ship")
             self.ship.update()
             print("updating screen")
-            self.bullets.update()
+            self._update_bullets()
             print("generating bullets")
-            self.update_screen()
+            self._update_screen()
             print ("checking framerate")
             self.clock.tick(60)
 
@@ -71,11 +71,16 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
-
-
-    def update_screen(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    def _update_bullets(self):
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
+    def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
             for bullet in self.bullets.sprites():
                 bullet.draw_bullet()
